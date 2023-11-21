91ed99d6bcff35fd52f4d92757f5b395007cf332
= <=>
2023-11-21 16:30:24 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,53 @@
+// Первое Домашнее задание 
+/*  let age = prompt("Введи свой возраст");
+
+function checkAge(age) {
+if (age >= 18 ) {
+  return "Добро пожаловать сэр";
+} else if (age <= 0 || isNaN(age) || age === "") {
+    return "Вы ввели неправильно сэр";
+} else {
+  return "Вы слишком малы сэр"
+}
+}
+
+let result = checkAge(age);
+console.log(result); 
+ */
+
+
+/* Второе домашнее задание
+let name1 = "Alex"
+let name2 = "George"
+let name3 = "Michael"     
+
+function longestName(names) {
+let longest = "";
+
+  for (let i = 0; i < names.length; i++) {
+    if (names[i].length > longest.length) {
+      longest = names[i];
+    }                                      
+  } 
+    return longest;                         
+}
+               
+  let names = [name1,name2,name3]
+
+  let longest = longestName(names)
+
+console.log(longest); */
+
+/* Третье домашнее задание
+function calculatorProporsia() {
+    let first = prompt("first");
+
+    let second = prompt("second");
+
+    let result = (second / first) * 100;
+    
+    return result;
+}
+  
+let proportion = calculatorProporsia();
+console.log("Выходит " + proportion + "%"); */
